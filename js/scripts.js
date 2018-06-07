3316e163b8197e367b2e72f038ee494251a34776
austin barr, curt caldwell and eliot charette <eliot@>
2018-06-07 15:35:26 -0700

    add final touches

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
-var arrayI = [];
-var arrayV =[];
-var arrayX = [];
-var arrayL = [];
-var arrayC = [];
-var arrayD = [];
-var arrayM = [];
-var numerals = ["","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+
 
 
 var roman = function(input){
   if (input > 3999 || input < 1) {
     return false;
  }
+ var arrayI = [];
+ var arrayV =[];
+ var arrayX = [];
+ var arrayXL = [];
+ var arrayL = [];
+ var arrayXC = [];
+ var arrayC = [];
+ var arrayCD = [];
+ var arrayD = [];
+ var arrayCM = [];
+ var arrayM = [];
+
+ var numerals = ["","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
 
  if (input >= 3000){
    input / 3000
@@ -27,6 +33,11 @@ if (input >= 1000){
   input / 1000
   arrayM.push('M')
   input -= 1000;
+}
+if (input >= 900){
+  input / 900
+  arrayCM.push('CM')
+  input -= 900;
 }
 if (input >= 500){
   input / 500
@@ -58,6 +69,11 @@ if (input >= 50){
   arrayL.push('L')
   input -= 50;
 }
+if (input >= 40){
+  input / 40
+  arrayXL.push('XL')
+  input -= 40;
+}
 if (input >= 30){
   input / 10
   arrayX.push('X')
@@ -74,15 +90,13 @@ if (input >= 10){
   input -= 10;
 }
 if(input >= 1) {
-  input = i
-    for (var i = 0; i <= numerals.length; i++) {
-    arrayI.push(numerals[i]);
-  
-  }
+      arrayI.push(numerals[input]);
 }
 
-var total = arrayM.concat(arrayD, arrayC, arrayL, arrayX, arrayI);
-console.log(total)
+var total = arrayM.concat(arrayCM, arrayD, arrayCD, arrayC, arrayXC, arrayL, arrayXL, arrayX, arrayI);
+var total = total.join('')
+$(".result").text(total)
+
 
   /*if (input === 1) {
     arrayI.push('I');
@@ -138,9 +152,9 @@ console.log(total)
 $(function(){
   $("form#number").submit(function(event){
     event.preventDefault();
-
+debugger;
     var input = parseInt($("input#number").val());
     roman(input);
-    console.log(input)
+
   });
 });
